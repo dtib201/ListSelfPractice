7a4e6c377704f4e2f6687dad54140d7ea0c0b3e5
Daniel Min Tibbotts <dtib201@mywhitecliffe.com>
2021-02-23 14:44:16 +1300

    Created VS Solution

---
+++ ListSelfPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListSelfPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
