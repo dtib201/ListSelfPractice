ddcc54a6fac7d97ed1907b51189d604df8f6d12f
Daniel Min Tibbotts <dtib201@mywhitecliffe.com>
2021-02-23 14:56:04 +1300

    Completed Skill Check 1: Lists

--- ListSelfPractice/Program.cs
+++ ListSelfPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace ListSelfPractice
 {
@@ -6,7 +8,29 @@ namespace ListSelfPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // practice declaring and initialising a generic list.
+            List<string> names = new List<string>();
+
+            names.Add("Fred");
+            names.Add("Barney");
+            names.Add("Wilma");
+            names.Add("Dino");
+
+            // Try adding elements of different data types to the List and note whether a compiler or a runtime error occurs.
+            //names.Add(12);  // Compile Error, expected string received int
+
+            // Practice displaying only specific List elements as well as displaying all the elements of the List.
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[3]);
+
+            Console.WriteLine();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine();
         }
     }
 }
